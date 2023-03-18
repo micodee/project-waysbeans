225ba6b6a22a9deeee52e14a6277ea917f277a94
micodee <martomhidayat>
2023-03-18 17:09:05 +0700

    success

--- server/pkg/mysql/mysql.go
+++ server/pkg/mysql/mysql.go
@@ -10,13 +10,13 @@ import (
 
 var ConnDB *gorm.DB
 
-var DB_HOST = os.Getenv("DB_HOST")
-var DB_USER = os.Getenv("DB_USER")
-var DB_PASSWORD = os.Getenv("DB_PASSWORD")
-var DB_NAME = os.Getenv("DB_NAME")
-var DB_PORT = os.Getenv("DB_PORT")
-
 func DatabaseInit() {
+	var DB_HOST = os.Getenv("DB_HOST")
+	var DB_USER = os.Getenv("DB_USER")
+	var DB_PASSWORD = os.Getenv("DB_PASSWORD")
+	var DB_NAME = os.Getenv("DB_NAME")
+	var DB_PORT = os.Getenv("DB_PORT")
+	
 	var err error
 	DBurl := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT)
 	ConnDB, err = gorm.Open(postgres.Open(DBurl), &gorm.Config{})
